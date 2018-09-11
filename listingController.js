328f87f194f2e6372f1277ef7234f1dcd131b686
theryop <theryop@outlook.com>
2018-09-10 23:23:32 -0400

    Added specified listing functionalities to the angular webpage

--- listingController.js
+++ listingController.js
@@ -2,13 +2,35 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
+    $scope.barFilter = function(curRow) {
+      return function(curRow)
+      {
+        if (typeof $scope.inputText == 'undefined') {
+          return curRow;
+        }
+        if (curRow.name.includes($scope.inputText) || curRow.code.includes($scope.inputText)) {
+          return curRow;
+        }
+      }
+    };
 
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
-     */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+
+    $scope.addListing = function() {
+      $scope.listings.push({
+        "code": $scope.newCode,
+        "name": $scope.newName,
+        "coordinates": {
+                "latitude": $scope.newLat, 
+                "longitude": $scope.newLong
+            }, 
+            "address": $scope.newAdd
+      });
+    };
+    $scope.deleteListing = function(index) {
+      $scope.listings.splice(index, 1);
+    };
+    $scope.showDetails = function(index) {
+      $scope.detailedInfo = $scope.listings[index];
+    };
   }
 ]);
